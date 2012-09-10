ed423c4d91a12a543392b12b229a6fa79a5585c8
mbryzhak <wbryzhak@gmail.com>
2012-09-10 14:02:53 -0700

    Initial commit

---
+++ src/main/java/com/master/sh_manager/App.java
@@ -0,0 +1,13 @@
+package com.master.sh_manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
